026dd84116ec346080d0d947c59c3dc1d4745091
Thomas Südbröcker <thomas.suedbroecker@de.ibm.com>
2024-08-16 15:14:03 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,210 @@
-# agent_tools_langchain_chatwatsonx
-The objective is to get a basic understanding of how to use the tools in AI agents by using the ChatWatsonx class.
+# Does it work to use WatsonxChat from langchain_ibm to implement an action that invokes functions?
+
+This repository is an example using the langchain_ibm implementation for action function calling with ChatWatsonx class.
+
+Related blog post: XXX
+
+## 0. Clone the repository
+
+```sh
+git clone https://github.com/thomassuedbroecker/agent_tools_langchain_chatwatsonx.git
+cd agent_tools_langchain_chatwatsonx
+```
+
+## 1. Environment Setup
+
+### Step 1: Generate a virtual Python environment
+
+```sh
+cd code
+python3 -m venv --upgrade-deps venv
+source venv/bin/activate
+```
+
+### Step 2: Install the needed libraries
+
+```sh 
+python3 -m pip install -qU langchain-ibm
+python3 -m pip install -qU langchain
+```
+
+### Step 3: Generate a `.env` file for the needed environment variables
+
+```sh
+cat env_example_template > .env
+```
+
+Insert the values for the two environment variables: 
+
+* `WATSONX_PROJECT_ID=YOUR_WATSONX_PROJECT_ID`
+* `IBMCLOUD_APIKEY=YOUR_KEY`
+
+Content of the environment file.
+
+```sh
+export IBMCLOUD_APIKEY=YOUR_KEY
+export IBMCLOUD_URL="https://iam.cloud.ibm.com/identity/token"
+
+# Watsonx
+export WATSONX_URL="https://eu-de.ml.cloud.ibm.com"
+export WATSONX_VERSION=2023-05-29
+export WATSONX_PROJECT_ID=YOUR_PROJECT_ID
+
+export WATSONX_MIN_NEW_TOKENS=1
+export WATSONX_MAX_NEW_TOKENS=300
+export WATSONX_LLM_NAME=mistralai/mixtral-8x7b-instruct-v01
+export WATSONX_INSTANCE_ID=YOUR_WATSONX_INSTANCE_ID
+```
+
+## 2. Execution
+
+### Step 5: Run the example
+
+```sh
+cd code
+bash example_agent_invocation.sh
+```
+
+* Output of the invocation:
+
+```sg
+======== TOOL ONLY ========
+1. Verify the tool schema definition
+=============================
+1. Tool name:
+today_maximal_temperature
+2. Tool description:
+provides the maximal temperature for today information for the given cities. 
+     Args:
+                cities: The parameter cities is a list e.g. [ "LA", "NY"].
+3. Tool arguments:
+{'cities': {'title': 'Cities', 'type': 'array', 'items': {'type': 'string'}}}
+4. Tool arguments title:
+Cities
+
+
+=============================
+
+
+2. Invoke the tool and print invocation result
+=============================
+Result:
+[{'city': 'Berlin', 'temperature_max': '29'}, {'city': 'NY', 'temperature_max': '28'}, {'city': 'Paris', 'temperature_max': '26'}]
+
+======== AGENT ONLY========
+Set LangChain Debug: False
+
+1. Load thhe environment variables
+{'project_id': 'YOUR_PROJECT', 'url': 'https://eu-de.ml.cloud.ibm.com', 'model_id': 'mistralai/mixtral-8x7b-instruct-v01', 'apikey': 'YOUR_IBMCLOUD_KEY'}
+
+
+2. Defined tools for langchain.tools.
+
+- The type to the tools:
+<class 'list'>
+- The tools list:
+[StructuredTool(name='current_temperature', description='provides the current temperature information for given cities. \n     Args:\n                cities: The parameter cities is a list e.g. ["LA", "NY"].', args_schema=<class 'pydantic.v1.main.current_temperatureSchema'>, func=<function current_temperature at 0x168614180>)]
+
+3. Define the model parameters
+{'decoding_method': 'greedy', 'max_new_tokens': 400, 'min_new_tokens': 1, 'temperature': 1.0}
+
+4. Create a ChatWatsonx instance for mistralai/mixtral-8x7b-instruct-v01, and not a WatsonxLLM, because to this instance a tool can't be bind.
+
+5. Define a prompt using a ChatPromptTemplate.
+
+-Prompt Template:
+<class 'langchain_core.prompts.chat.ChatPromptTemplate'>
+
+input_variables=['input'] optional_variables=['agent_scratchpad', 'chat_history'] input_types={'chat_history': typing.List[typing.Union[langchain_core.messages.ai.AIMessage, langchain_core.messages.human.HumanMessage, langchain_core.messages.chat.ChatMessage, langchain_core.messages.system.SystemMessage, langchain_core.messages.function.FunctionMessage, langchain_core.messages.tool.ToolMessage]], 'agent_scratchpad': typing.List[typing.Union[langchain_core.messages.ai.AIMessage, langchain_core.messages.human.HumanMessage, langchain_core.messages.chat.ChatMessage, langchain_core.messages.system.SystemMessage, langchain_core.messages.function.FunctionMessage, langchain_core.messages.tool.ToolMessage]]} partial_variables={'chat_history': [], 'agent_scratchpad': []} messages=[SystemMessagePromptTemplate(prompt=PromptTemplate(input_variables=[], template="You are a weather expert. If the question is not about the weather, say: I don't know.")), MessagesPlaceholder(variable_name='chat_history', optional=True), HumanMessagePromptTemplate(prompt=PromptTemplate(input_variables=['input'], template='{input}')), MessagesPlaceholder(variable_name='agent_scratchpad', optional=True)]
+
+6. Create a tool calling agent based on parameters: the watsonx_chat_with_tools, the tool, and the prompt.
+
+7. Create an AgentExecutor.
+
+8. Execute the agent using the AgentExecutor by providing a question as input to invoke the model.
+
+
+
+> Entering new AgentExecutor chain...
+
+
+```json
+{
+    "type": "function",
+    "function": {
+        "name": "current_temperature",
+        "arguments": {
+            "cities": ["LA", "NY"]
+        }
+    }
+}
+```
+
+</endoftext>
+
+> Finished chain.
+
+
+> Entering new AgentExecutor chain...
+
+
+```json
+{
+    "type": "function",
+    "function": {
+        "name": "current_temperature",
+        "arguments": {
+            "cities": ["Berlin"]
+        }
+    }
+}
+```
+</endoftext>
+
+> Finished chain.
+
+
+> Entering new AgentExecutor chain...
+
+
+```json
+{
+    "type": "function",
+    "function": {
+        "name": "Final Answer",
+        "arguments": {
+            "output": "I don't know."
+        }
+    }
+}
+</endoftext>
+
+> Finished chain.
+
+
+> Entering new AgentExecutor chain...
+
+
+```json
+{
+    "type": "function",
+    "function": {
+        "name": "Final Answer",
+        "arguments": {
+            "output": "The term 'weather' refers to the state of the atmosphere at a particular place and time, with respect to temperature, humidity, cloudiness, wind, and atmospheric pressure."
+        }
+    }
+}
+```
+</endoftext>
+
+> Finished chain.
+9. Results
+
+|                 agent question                     |                  agent anwser                      |
+|----------------------------------------------------|----------------------------------------------------|
+|Which city is hotter today: LA or NY?_______________|function:current_temperature________________________|
+|What is the temperature today in Berlin?____________|function:current_temperature________________________|
+|How to win a soccer game?___________________________|function:Final Answer_______________________________|
+|What is the official definition of the term weather?|function:Final Answer_______________________________|
+```
---
+++ code/agent_example.py
@@ -0,0 +1,207 @@
+import argparse
+import json
+import requests
+
+from langchain_ibm import ChatWatsonx
+from langchain.agents import create_tool_calling_agent, AgentExecutor
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.tools import tool
+from langchain.globals import set_debug
+from typing import List
+
+from modules.load_env import load_watson_x_env_min, load_ibmcloud_env
+
+# ******************************************
+# Functions
+
+@tool (response_format="content")
+def current_temperature(cities: List[str]) -> List[str]:
+     """provides the current temperature information for given cities. 
+     Args:
+                cities: The parameter cities is a list e.g. ["LA", "NY"].
+     """
+     base_weather_url="https://wttr.in/"
+     cities_input = cities
+     cities_output = []
+     
+     for city in cities_input:
+          # Ensure to get the JSON format: '?format=j1'
+          city_temp_url = base_weather_url + city + "?format=j1"
+          response = requests.get(city_temp_url)
+          if (response.status_code == 200):      
+              # convert from byte to text
+              byte_content = response.content
+              text_content = byte_content.decode("utf-8")
+              
+              # load json
+              content = json.loads(text_content)
+              print(f"{content}")
+
+              # extract temperature
+              temperature = content['current_condition'][0]['temp_C']
+              cities_output.append({"city": city, "temperature":temperature})
+          else:
+              cities_output.append({"city": f"{city} ERROR", "temperature":0})
+                    
+     return cities_output              
+
+@tool (response_format="content")
+def today_maximal_temperature(cities: List[str]) -> List[str]:
+     """provides the maximal temperature for today information for the given cities. 
+     Args:
+                cities: The parameter cities is a list e.g. [ "LA", "NY"].
+     """
+     base_weather_url="https://wttr.in/"
+     cities_input = cities
+     cities_output = []
+     
+     for city in cities_input:
+          # Ensure to get the JSON format: '?format=j1'
+          city_temp_url = base_weather_url + city + "?format=j1"
+          response = requests.get(city_temp_url)
+          if (response.status_code == 200):      
+              # convert from byte to text
+              byte_content = response.content
+              text_content = byte_content.decode("utf-8")
+              
+              # load json
+              content = json.loads(text_content)
+
+              # extract temperature
+              temperature_max = content['weather'][0]['maxtempC']
+              cities_output.append({"city": city, "temperature_max":temperature_max})
+          else:
+              cities_output.append({"city": f"{city} ERROR", "temperature_max":0}) 
+
+     return cities_output
+            
+def load_env():
+        
+        ibmcloud_apikey , validation = load_ibmcloud_env()
+        if( validation == False):
+            return {  "apikey" : "ERROR",
+                      "token" : "ERROR", 
+                      "project_id" : "ERROR",
+                      "url": "ERROR",
+                      "model_id": "ERROR"
+                    }
+        else:
+             apikey = ibmcloud_apikey['IBMCLOUD_APIKEY']
+                   
+        watson_x_env, validation = load_watson_x_env_min()
+        
+        if validation == False:
+            return { "apikey" : "ERROR", 
+                     "project_id" : "ERROR",
+                     "url": "ERROR",
+                     "model_id": "ERROR"
+                    }
+        else:
+             project_id = watson_x_env['WATSONX_PROJECT_ID']
+             url = watson_x_env['WATSONX_URL']
+             model_id = watson_x_env['WATSONX_LLM_NAME']
+                        
+        return { "project_id" : project_id,
+                 "url": url,
+                 "model_id": model_id,
+                 "apikey": apikey }
+
+def agent_tools_calling():
+     
+     environment = load_env()
+     print(f"1. Load thhe environment variables\n{environment}\n\n")  
+
+     tool = [current_temperature]
+     print(f"2. Defined tools for langchain.tools.\n")
+     print(f"- The type to the tools:\n{type(tool)}")
+     print(f"- The tools list:\n{tool}\n")
+        
+     parameters = {
+            "decoding_method": "greedy",
+            "max_new_tokens": 400,
+            "min_new_tokens": 1,
+            "temperature": 1.0
+     }
+     print(f"3. Define the model parameters\n{parameters}\n")
+        
+     print(f"4. Create a ChatWatsonx instance for {environment['model_id']}, and not a WatsonxLLM, because to this instance a tool can't be bind.\n")
+     watsonx_chat =  ChatWatsonx( model_id=environment['model_id'],
+            url=environment['url'],
+            project_id=environment['project_id'],
+            apikey= environment['apikey'],
+            params=parameters
+     )
+
+     print(f"5. Define a prompt using a ChatPromptTemplate.\n")            
+     prompt_template = ChatPromptTemplate.from_messages(
+       [
+              ("system", system_prompt_load()),
+              ("placeholder", "{chat_history}"),
+              ("human", "{input}"),
+              ("placeholder", "{agent_scratchpad}"),
+       ]
+     )
+     print(f"-Prompt Template:\n{type(prompt_template)}\n\n{prompt_template}\n")
+     print(f"6. Create a tool calling agent based on parameters: the watsonx_chat_with_tools, the tool, and the prompt.\n")  
+     agent = create_tool_calling_agent(watsonx_chat, tool, prompt_template)
+
+     print(f"7. Create an AgentExecutor.\n")  
+     agent_executor = AgentExecutor(agent=agent, tools=tool, verbose=True)
+     
+     print(f"8. Execute the agent using the AgentExecutor by providing a question as input to invoke the model.\n")
+     questions = ["Which city is hotter today: LA or NY?", 
+                  "What is the temperature today in Berlin?",
+                  "How to win a soccer game?",
+                  "What is the official definition of the term weather?"]
+     
+     results = []
+     for question in questions:
+          response = agent_executor.invoke({"input": question,
+                                            "include_run_info": True})
+          text_json = response['output'].replace('```json\n', '').replace('```\n', '').replace('</endoftext>', '')
+          content = json.loads(text_json)
+          data = { "question": question, "answer":  content['type'] + ':' +content['function']['name'] }
+          results.append(data)
+     
+     print(f"9. Results\n")
+     print(f"|                 agent question                     |                  agent anwser                      |")
+     print(f"|----------------------------------------------------|----------------------------------------------------|")
+     for result in results:
+          print(f"|{result['question'].ljust(52, '_')}|{result['answer'].ljust(52, '_')}|")
+     
+     return result
+
+def system_prompt_load():
+      system_prompt = """You are a weather expert. If the question is not about the weather, say: I don't know."""     
+      return system_prompt
+
+# ******************************************
+# Execution
+def main(args):
+     city_list = ["Berlin", "NY", "Paris"]
+
+     print(f"======== TOOL ONLY ========")
+     print(f"1. Verify the tool schema definition")
+     print(f"=============================")
+     print(f"1. Tool name:\n{ today_maximal_temperature.name}")
+     print(f"2. Tool description:\n{today_maximal_temperature.description}")
+     print(f"3. Tool arguments:\n{today_maximal_temperature.args}")
+     print(f"4. Tool arguments title:\n{today_maximal_temperature.args['cities']['title']}\n")
+     
+     print(f"\n=============================")
+     print(f"\n\n2. Invoke the tool and print invocation result")
+     print(f"=============================")
+     argument = {"cities": city_list}
+     result = today_maximal_temperature.invoke(argument)
+     print(f"Result:\n{result}\n")
+     
+     print(f"======== AGENT ONLY========")
+     debug_value = False
+     print(f"Set LangChain Debug: {debug_value}\n")
+     set_debug(debug_value) #Set global debug for LangChain
+     agent_tools_calling()
+
+if __name__ == "__main__":  
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+    main(args)
